0f48ef80cd95cbce7fd4a0b57304ffac1e7afdf7
Dilawar Javaid <dilawar.javaid864@gmail.com>
2025-05-06 11:07:56 -0700

    create QuizApp class and initialize quiz state variables. fetch quiz data from remote JSON and display quiz title buttons

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import json
 import tkinter as tk
 import requests
 from tkinter import messagebox
+
+#Define the QuizApp Class and Initialize UI
+class QuizApp:
+    def __init__(self, root):
+        # Fetch Quiz Data from a Remote URL
+        url = "https://raw.githubusercontent.com/arditsulceteaching/hosted_files/main/geo.json"
+        response = requests.get(url)
+
+
